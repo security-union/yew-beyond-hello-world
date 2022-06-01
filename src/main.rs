44500530c0c4cecde17323a8dc207db9a78292eb
Dario Lencina Talarico <dario@armore.dev>
2022-06-01 00:22:31 -0400

    save

--- src/main.rs
+++ src/main.rs
@@ -1,61 +1,85 @@
-use std::result;
+mod msg_ctx;
 
-use web_sys::{window, console, MediaStreamConstraints};
-use yew::prelude::*;
-use wasm_bindgen_futures::JsFuture;
-use js_sys::{Array, Object, JsString, Boolean};
+use js_sys::Array;
+use js_sys::Boolean;
+use msg_ctx::{MessageContext, MessageProvider};
+use wasm_bindgen::JsCast;
 use wasm_bindgen::JsValue;
+use wasm_bindgen_futures::JsFuture;
+use web_sys::*;
+use yew::prelude::*;
 
 #[function_component(App)]
 fn app() -> Html {
     html!(
-        <div>
+        <MessageProvider>
             <VideoReader/>
             <VideoRenderer/>
-        </div>
+        </MessageProvider>
     )
 }
 
 #[function_component(VideoReader)]
 fn video_reader() -> Html {
+    use_effect_with_deps(
+        move |_| {
+            let navigator = window().unwrap().navigator();
+            let media_devices = navigator.media_devices().unwrap();
+
+            wasm_bindgen_futures::spawn_local(async move {
+                let video_element = window()
+                    .unwrap()
+                    .document()
+                    .unwrap()
+                    .get_element_by_id("webcam")
+                    .unwrap()
+                    .unchecked_into::<HtmlVideoElement>();
+                let mut constraints = MediaStreamConstraints::new();
+                constraints.video(&Boolean::from(true));
+                let devices_future = media_devices
+                    .get_user_media_with_constraints(&constraints)
+                    .unwrap();
+                let device = JsFuture::from(devices_future)
+                    .await
+                    .unwrap()
+                    .unchecked_into::<MediaStream>();
+                console::log_1(&device);
+                console::log_1(&video_element);
+                video_element.set_src_object(Some(&device));
+                let video_tracks = device
+                    .get_video_tracks()
+                    .find(&mut |_: JsValue, _: u32, _: Array| true);
+            });
+            || ()
+        },
+        (),
+    );
+
+    let msg_ctx = use_context::<MessageContext>().unwrap();
+    let onclick = Callback::from(move |_| msg_ctx.dispatch("Message Received.".to_string()));
+
     html!(
         <div>
             {"video reader"}
+            <video autoplay={true} id="webcam"></video>
+            <button {onclick}>
+                {"PRESS ME"}
+            </button>
         </div>
     )
 }
 
 #[function_component(VideoRenderer)]
 fn video_renderer() -> Html {
-    use_effect_with_deps(move |_| {
-        let navigator = window().unwrap().navigator();
-        let media_devices = navigator.media_devices().unwrap();
-        
-        wasm_bindgen_futures::spawn_local(async move {
-            let video_element = web_sys::window().unwrap().document().unwrap().get_element_by_id("webcam").unwrap();
-            let property = JsString::from("srcObject");
-            let mut constraints = MediaStreamConstraints::new();
-            constraints.video(&Boolean::from(true));
-            let devices2 = media_devices.get_user_media_with_constraints(&constraints).unwrap();
-            let device = JsFuture::from(devices2).await.unwrap();
-            console::log_1(&device);
-            console::log_1(&video_element);
-            let set_result = js_sys::Reflect::set(&video_element, &property, &device).map_err(|e| {
-                console::error_1(&e);
-            }).unwrap();
-            console::log_1(&Boolean::from(set_result));
-        });
-        || ()
-    }, ());
-
+    let msg_ctx = use_context::<MessageContext>().unwrap();
+    let message = msg_ctx.inner.to_owned();
     html!(
         <div>
             {"video renderer"}
-            <video autoplay={true} id="webcam"></video>
+             <h1>{ message }</h1>
         </div>
     )
 }
-
 
 fn main() {
     yew::start_app::<App>();
